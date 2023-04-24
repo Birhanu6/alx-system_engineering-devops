d9b3ff6fbe6a22df6cd6fff05d72515e3d15d5de
birhanu6 <birhanusisayh@gmail.com>
2023-04-24 12:26:55 -0700

    all taskes done

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+This program write user data
+to json file
+'''
+
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    emp_id = int(sys.argv[1])
+    users_url = "https://jsonplaceholder.typicode.com/users/{}"
+    emp_data = requests.get(users_url.format(emp_id)).json()
+    emp_uname = emp_data.get('username')
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    filename = str(emp_id) + '.json'
+    tasks_obj = []
+    with open(filename, mode='w') as f:
+        for task in tasks:
+            if task.get('userId') == emp_id:
+                task_obj = {"task": task.get('title'),
+                            "completed": task.get('completed'),
+                            "username": emp_uname}
+                tasks_obj.append(task_obj)
+        json_obj = {str(emp_id): tasks_obj}
+        json.dump(json_obj, f)
